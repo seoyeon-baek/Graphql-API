47be210833c1eca0aaee26d330e6da2e8c95d4c7
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-11 01:26:03 +0900

    Add: db

---
+++ graphql/db.js
@@ -0,0 +1,32 @@
+export const people = [
+  {
+    id: "1",
+    name: "Seoyeon",
+    age: 19,
+    gender: "male",
+  },
+  {
+    id: "1",
+    name: "amy",
+    age: 19,
+    gender: "male",
+  },
+  {
+    id: "1",
+    name: "brocola",
+    age: 19,
+    gender: "male",
+  },
+  {
+    id: "1",
+    name: "siumin",
+    age: 19,
+    gender: "male",
+  },
+  {
+    id: "5",
+    name: "xian",
+    age: 19,
+    gender: "male",
+  },
+];
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,6 @@
-const seoyeon = {
-  name: "Seoyeon",
-  age: 19,
-  gender: "male",
-};
-
 const resolvers = {
   Query: {
-    person: () => seoyeon, //person 호출 시 seoyeon을 리턴한다.
+    people: () => people, //person 호출 시 people을 리턴한다.
   },
 };
 
